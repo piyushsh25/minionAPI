3a47ead62620874ec696bd325d001cc17aef2798
piyushpoudyal <72690317+piyushpoudyal@users.noreply.github.com>
2021-11-29 23:39:47 +0530

    feature: i/o processing done

---
+++ app.js
@@ -0,0 +1,10 @@
+var btnTranslate= document.querySelector("#btn-translate");
+var textInput=document.querySelector("#txt-input");
+var outputDiv=document.querySelector("#output")
+
+
+btnTranslate.addEventListener("click",function clickEventHandler(){
+outputDiv.innerHTML="fvjfm "+textInput.value;
+})  
+
+ 
