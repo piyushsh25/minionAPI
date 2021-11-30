1be58a9858ede5ea59c0b4283ef5a9fa9e929e2c
piyushpoudyal <72690317+piyushpoudyal@users.noreply.github.com>
2021-11-30 07:41:45 +0530

    bug: directly assigned text to textInput in function

--- app.js
+++ app.js
@@ -2,9 +2,22 @@ var btnTranslate= document.querySelector("#btn-translate");
 var textInput=document.querySelector("#txt-input");
 var outputDiv=document.querySelector("#output")
 
+var serverURL="https://api.funtranslations.com/translate/minion.json";
+function getTranslationURL(textInput){
+    return serverURL+"?"+"text="+textInput;
+}
 
 btnTranslate.addEventListener("click",function clickEventHandler(){
-outputDiv.innerHTML="fvjfm "+textInput.value;
+// outputDiv.innerHTML="fvjfm "+textInput.value;
+var txtInput=textInput.value;
+
+fetch(getTranslationURL(txtInput))
+.then(response => response.json())
+.then(json =>{
+    var translatedText= json.contents.translated;
+    outputDiv.innerText=translatedText;
+    console.log(json.contents.translated)})
+.catch(console.log("error occured"+ Error))
 })  
 
  
